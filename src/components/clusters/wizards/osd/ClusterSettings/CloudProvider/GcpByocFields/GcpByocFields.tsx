c5461bfc8a4b0cf5abdbcba3a5826070bfb13b22
Dylan Cooper <dcooper@redhat.com>
2025-08-25 13:02:29 -0600

    flipped order of WIF and Service Account buttons

--- src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields/GcpByocFields.tsx
+++ src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields/GcpByocFields.tsx
@@ -34,7 +34,7 @@ import { WorkloadIdentityFederationPrerequisites } from '~/components/clusters/w
 import { GCPAuthType } from '~/components/clusters/wizards/osd/ClusterSettings/CloudProvider/types';
 import { FieldId } from '~/components/clusters/wizards/osd/constants';
 import ExternalLink from '~/components/common/ExternalLink';
-import { OSD_GCP_WIF } from '~/queries/featureGates/featureConstants';
+import { GCP_WIF_DEFAULT, OSD_GCP_WIF } from '~/queries/featureGates/featureConstants';
 import { useFeatureGate } from '~/queries/featureGates/useFetchFeatureGate';
 import { SubscriptionCommonFieldsCluster_billing_model as SubscriptionCommonFieldsClusterBillingModel } from '~/types/accounts_mgmt.v1';
 
@@ -47,6 +47,7 @@ export const GcpByocFields = (props: GcpByocFieldsProps) => {
   } = useFormState();
 
   const isWifEnabled = useFeatureGate(OSD_GCP_WIF);
+  const isWifDefaultEnabled = useFeatureGate(GCP_WIF_DEFAULT);
 
   const gcpTitle = 'Have you prepared your Google account?';
   const gcpText = `To prepare your account, accept the Google Cloud Terms and Agreements. If you've already accepted the terms, you can continue to complete OSD prerequisites.`;
@@ -70,6 +71,46 @@ export const GcpByocFields = (props: GcpByocFieldsProps) => {
     // when auth type changes, prerequisites change too, so resetting the acknowledgment state
     setFieldValue(FieldId.AcknowledgePrereq, false);
   };
+
+  const authButtons = (
+    <ToggleGroup aria-label="Authentication type">
+      {isWifDefaultEnabled ? (
+        <>
+          <ToggleGroupItem
+            text="Workload Identity Federation"
+            key={0}
+            buttonId={GCPAuthType.WorkloadIdentityFederation}
+            isSelected={authType === GCPAuthType.WorkloadIdentityFederation}
+            onChange={handleAuthChange}
+          />
+          <ToggleGroupItem
+            text="Service Account"
+            key={1}
+            buttonId={GCPAuthType.ServiceAccounts}
+            isSelected={authType === GCPAuthType.ServiceAccounts}
+            onChange={handleAuthChange}
+          />
+        </>
+      ) : (
+        <>
+          <ToggleGroupItem
+            text="Service Account"
+            key={0}
+            buttonId={GCPAuthType.ServiceAccounts}
+            isSelected={authType === GCPAuthType.ServiceAccounts}
+            onChange={handleAuthChange}
+          />
+          <ToggleGroupItem
+            text="Workload Identity Federation"
+            key={1}
+            buttonId={GCPAuthType.WorkloadIdentityFederation}
+            isSelected={authType === GCPAuthType.WorkloadIdentityFederation}
+            onChange={handleAuthChange}
+          />
+        </>
+      )}
+    </ToggleGroup>
+  );
 
   return (
     <Form isWidthLimited onSubmit={(e) => e.preventDefault()}>
@@ -115,22 +156,7 @@ export const GcpByocFields = (props: GcpByocFieldsProps) => {
                 </Popover>
               }
             >
-              <ToggleGroup aria-label="Authentication type">
-                <ToggleGroupItem
-                  text="Service Account"
-                  key={0}
-                  buttonId={GCPAuthType.ServiceAccounts}
-                  isSelected={authType === GCPAuthType.ServiceAccounts}
-                  onChange={handleAuthChange}
-                />
-                <ToggleGroupItem
-                  text="Workload Identity Federation"
-                  key={1}
-                  buttonId={GCPAuthType.WorkloadIdentityFederation}
-                  isSelected={authType === GCPAuthType.WorkloadIdentityFederation}
-                  onChange={handleAuthChange}
-                />
-              </ToggleGroup>
+              {authButtons}
             </FormGroup>
           </FlexItem>
         )}
