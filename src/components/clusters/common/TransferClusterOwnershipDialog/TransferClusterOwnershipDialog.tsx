1dac6f0fc5be69ea6430aaf19b1dd316b6361d5a
Dave Taylor <dtaylor@redhat.com>
2025-09-06 16:10:00 -0400

    all access.redhat.com urls consolidated
    initial url consolidation of support links & errors.ts
    
    update link and test for OfflineTokensAlert.tsx
    
    Update TransferClusterOwnershipDialog.tsx's link and added test
    
    ArchiveClusterDialog.tsx link & test
    
    SupportStatus & UpgradeSettingsFields links and tests
    
    ClusterStatusMonitor.jsx & ClusterListTable.jsx link changes and tests
    
    next 2 components links and tests
    
    next 2
    
    next 2
    
    next 2

--- src/components/clusters/common/TransferClusterOwnershipDialog/TransferClusterOwnershipDialog.tsx
+++ src/components/clusters/common/TransferClusterOwnershipDialog/TransferClusterOwnershipDialog.tsx
@@ -7,6 +7,7 @@ import { useAddNotification } from '@redhat-cloud-services/frontend-components-n
 import getClusterName from '~/common/getClusterName';
 import { ocmBaseName } from '~/common/routing';
 import Modal from '~/components/common/Modal/Modal';
+import supportLinks from '~/common/supportLinks.mjs';
 import { useToggleSubscriptionReleased } from '~/queries/ClusterActionsQueries/useToggleSubscriptionReleased';
 import { useGlobalState } from '~/redux/hooks';
 import { Subscription, SubscriptionCommonFieldsStatus } from '~/types/accounts_mgmt.v1';
@@ -16,8 +17,6 @@ import ErrorBox from '../../../common/ErrorBox';
 import ExternalLink from '../../../common/ExternalLink';
 import { closeModal } from '../../../common/Modal/ModalActions';
 import modals from '../../../common/Modal/modals';
-
-const CHANGE_PULL_SECRET_URL = 'https://access.redhat.com/solutions/4902871';
 
 type TransferClusterOwnershipDialogProps = {
   onClose: () => void;
@@ -128,7 +127,7 @@ const TransferClusterOwnershipDialog = ({ onClose }: TransferClusterOwnershipDia
             <Content component={ContentVariants.ol}>
               <Content component="li">Initiate transfer</Content>
               <Content component="li">
-                <ExternalLink href={CHANGE_PULL_SECRET_URL}>
+                <ExternalLink href={supportLinks.PULL_SECRET_CHANGE_KB}>
                   Change the cluster&apos;s pull secret
                 </ExternalLink>{' '}
                 within 5 days
