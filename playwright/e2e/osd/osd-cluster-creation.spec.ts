1e6c9dfc0dce201c5e9060724668f011e373ca80
jmekkatt <jmekkatt@redhat.com>
2025-09-11 19:34:07 +0530

    updated ci failing tests

--- playwright/e2e/osd/osd-cluster-creation.spec.ts
+++ playwright/e2e/osd/osd-cluster-creation.spec.ts
@@ -17,7 +17,6 @@ let globalNavPage: GlobalNavPage;
 
 test.describe.serial('OSD cluster tests', { tag: ['@ci'] }, () => {
   test.beforeAll(async ({ browser }) => {
-    // Setup: auth + navigate to cluster list
     const setup = await setupTestSuite(browser, 'cluster-list');
 
     sharedContext = setup.context;
@@ -38,117 +37,77 @@ test.describe.serial('OSD cluster tests', { tag: ['@ci'] }, () => {
     await cleanupTestSuite(sharedContext);
   });
 
-  test.describe.serial('Create OSD cluster on AWS flow', () => {
-    test('navigates to create OSD cluster', async () => {
-      await sharedPage.getByTestId('create_cluster_btn').click();
-      await createClusterPage.isCreateClusterPage();
-      await createOSDWizardPage.osdCreateClusterButton().click();
-      await createOSDWizardPage.isCreateOSDPage();
-    });
-
-    test('shows an error with invalid and empty names', async () => {
-      await createOSDWizardPage.isBillingModelScreen();
-      // select Red Hat account cloud option (previously default value)
-      await sharedPage.locator(createOSDWizardPage.billingModelRedHatCloudAccountOption).click();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await sharedPage.getByTestId('aws-provider-card').click();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-
-      await createOSDWizardPage.isClusterDetailsScreen();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).scrollIntoViewIfNeeded();
-      await sharedPage
-        .locator(createOSDWizardPage.clusterNameInput)
-        .fill('aaaaaaaaaaaaaaaa-aaaaaaaaaaaaaaaa-aaaaaaaaaaaaaaaa-aaaaaaaa');
-      await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
-        '1 - 54 characters',
-      );
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
-      await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toHaveCount(4);
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('a*a');
-      await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
-        'Consist of lower-case alphanumeric',
-      );
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('9a');
-      await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
-        'Start with a lower-case alphabetic',
-      );
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('a*');
-      await expect(
-        sharedPage.locator(createOSDWizardPage.clusterNameInputError).last(),
-      ).toContainText('End with a lower-case alphanumeric');
-    });
-
-    test.skip('fills OSD wizard but does not really create an OSD cluster', async () => {
-      // TODO: This test fails because the page state is not preserved between serial tests
-      // The test ends up back on the cluster list page instead of continuing from the OSD wizard
-      // This needs investigation into why the page navigation is not maintained in Playwright serial tests
-      console.log('Third test URL:', sharedPage.url());
-      // Ensure we're on the cluster details screen
-      await createOSDWizardPage.isClusterDetailsScreen();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
-      await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill(clusterName);
-      await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toHaveCount(0);
-
-      // click "next" until the cluster is created :)
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await createOSDWizardPage.isMachinePoolScreen();
-      await expect(sharedPage.locator('.spinner-loading-text')).not.toBeVisible();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await createOSDWizardPage.isNetworkingScreen();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await createOSDWizardPage.isCIDRScreen();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await createOSDWizardPage.isClusterUpdatesScreen();
-      await sharedPage.locator(createOSDWizardPage.primaryButton).click();
-      await createOSDWizardPage.isReviewScreen();
-    });
-  });
-});
-
-test.describe('OSD Trial cluster tests', () => {
-  let trialSharedContext: BrowserContext;
-  let trialSharedPage: Page;
-  let trialClusterListPage: ClusterListPage;
-  let trialCreateClusterPage: CreateClusterPage;
-  let trialCreateOSDWizardPage: CreateOSDWizardPage;
-  let trialGlobalNavPage: GlobalNavPage;
-
-  test.beforeAll(async ({ browser }) => {
-    // Setup: auth + navigate to cluster list
-    const setup = await setupTestSuite(browser, 'cluster-list');
-
-    trialSharedContext = setup.context;
-    trialSharedPage = setup.page;
-
-    // Initialize page objects for this test suite
-    trialClusterListPage = new ClusterListPage(trialSharedPage);
-    trialCreateClusterPage = new CreateClusterPage(trialSharedPage);
-    trialCreateOSDWizardPage = new CreateOSDWizardPage(trialSharedPage);
-    trialGlobalNavPage = new GlobalNavPage(trialSharedPage);
-
-    // Wait for cluster list data to load
-    await trialClusterListPage.waitForDataReady();
+  test('Create OSD cluster on AWS flow- navigates to create OSD cluster', async () => {
+    await sharedPage.getByTestId('create_cluster_btn').click();
+    await createClusterPage.isCreateClusterPage();
+    await createOSDWizardPage.osdCreateClusterButton().click();
+    await createOSDWizardPage.isCreateOSDPage();
   });
 
-  test.afterAll(async () => {
-    await cleanupTestSuite(trialSharedContext);
+  test('shows an error with invalid and empty names', async () => {
+    await createOSDWizardPage.isBillingModelScreen();
+    // select Red Hat account cloud option (previously default value)
+    await sharedPage.locator(createOSDWizardPage.billingModelRedHatCloudAccountOption).click();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await sharedPage.getByTestId('aws-provider-card').click();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+
+    await createOSDWizardPage.isClusterDetailsScreen();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).scrollIntoViewIfNeeded();
+    await sharedPage
+      .locator(createOSDWizardPage.clusterNameInput)
+      .fill('aaaaaaaaaaaaaaaa-aaaaaaaaaaaaaaaa-aaaaaaaaaaaaaaaa-aaaaaaaa');
+    await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
+      '1 - 54 characters',
+    );
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
+    await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toHaveCount(4);
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('a*a');
+    await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
+      'Consist of lower-case alphanumeric',
+    );
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('9a');
+    await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toContainText(
+      'Start with a lower-case alphabetic',
+    );
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill('a*');
+    await expect(
+      sharedPage.locator(createOSDWizardPage.clusterNameInputError).last(),
+    ).toContainText('End with a lower-case alphanumeric');
   });
 
-  test.describe('View Create OSD Trial cluster page', () => {
-    test('navigates to create OSD Trial cluster and CCS is selected', async () => {
-      await trialGlobalNavPage.breadcrumbItem('Cluster List').click({ force: true });
+  test('View Create OSD Trial cluster page - navigates to create OSD Trial cluster and CCS is selected', async () => {
+    await sharedPage.goto('create');
+    await createOSDWizardPage.waitAndClick(createOSDWizardPage.osdTrialCreateClusterButton());
+    await createOSDWizardPage.isCreateOSDTrialPage();
+    await createOSDWizardPage.isTrailDefinitionScreen();
+  });
 
-      await trialClusterListPage.waitForDataReady();
+  test.skip('fills OSD wizard but does not really create an OSD cluster', async () => {
+    // TODO: This test fails because the page state is not preserved between serial tests
+    // The test ends up back on the cluster list page instead of continuing from the OSD wizard
+    // This needs investigation into why the page navigation is not maintained in Playwright serial tests
+    console.log('Third test URL:', sharedPage.url());
+    // Ensure we're on the cluster details screen
+    await createOSDWizardPage.isClusterDetailsScreen();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).clear();
+    await sharedPage.locator(createOSDWizardPage.clusterNameInput).fill(clusterName);
+    await expect(sharedPage.locator(createOSDWizardPage.clusterNameInputError)).toHaveCount(0);
 
-      await trialSharedPage.getByTestId('create_cluster_btn').click();
-      await trialCreateClusterPage.isCreateClusterPage();
-      await trialSharedPage.getByTestId('osd-create-trial-cluster').click({ force: true });
-      await trialCreateOSDWizardPage.isCreateOSDTrialPage();
-      await expect(trialSharedPage.locator(trialCreateOSDWizardPage.CCSSelected)).toBeVisible();
-      await expect(trialSharedPage.locator(trialCreateOSDWizardPage.TrialSelected)).toBeVisible();
-    });
+    // click "next" until the cluster is created :)
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await createOSDWizardPage.isMachinePoolScreen();
+    await expect(sharedPage.locator('.spinner-loading-text')).not.toBeVisible();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await createOSDWizardPage.isNetworkingScreen();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await createOSDWizardPage.isCIDRScreen();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await createOSDWizardPage.isClusterUpdatesScreen();
+    await sharedPage.locator(createOSDWizardPage.primaryButton).click();
+    await createOSDWizardPage.isReviewScreen();
   });
 });
