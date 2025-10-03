e2a4a7746ce72aa97d0768555d17335beec0c073
Dave Taylor <dtaylor@redhat.com>
2025-10-03 14:04:25 -0400

    added for divide by 3 divisibility check

--- src/components/clusters/common/ScaleSection/AutoScaleSection/AutoScaleHelper.js
+++ src/components/clusters/common/ScaleSection/AutoScaleSection/AutoScaleHelper.js
@@ -53,7 +53,8 @@ export const getNodesCount = (isBYOC, isMultiAz, asString) => {
 
 export const getMinReplicasCount = (isBYOC, isMultiAz, asString, isHypershiftSelected = false) => {
   const nodesCount = getNodesCount(isBYOC, isMultiAz, false);
-  const minReplicas = isMultiAz && !isHypershiftSelected ? nodesCount / 3 : nodesCount;
+  const minReplicas =
+    isMultiAz && !isHypershiftSelected && nodesCount % 3 === 0 ? nodesCount / 3 : nodesCount;
   return asString ? `${minReplicas}` : minReplicas;
 };
 
