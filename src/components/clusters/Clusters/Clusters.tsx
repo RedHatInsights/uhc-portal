505b1fbeb0a387ab4f305c04209ad4370ff78b88
zherman0 <zherman@redhat.com>
2025-10-17 12:39:18 -0600

    Create tabbed clusters page for list and requests

---
+++ src/components/clusters/Clusters/Clusters.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { Tab, Tabs, TabTitleText } from '@patternfly/react-core';
+
+import { ClustersPageHeader } from './ClustersPageHeader';
+
+const DEFAULT_TAB = 'list';
+
+export const Clusters = () => {
+  const someReadOnly = false;
+  const showSpinner = false;
+  const error = false;
+  const refresh = () => {};
+  const location = useLocation();
+
+  // Get tab key from hash, or use default
+  const getTabFromHash = (): string => {
+    const hash = location.hash.replace('#', '');
+    return hash || DEFAULT_TAB;
+  };
+
+  const [activeTabKey, setActiveTabKey] = React.useState<string>(getTabFromHash());
+
+  // Update active tab when hash changes
+  React.useEffect(() => {
+    const hash = location.hash.replace('#', '');
+    const tabKey = hash || DEFAULT_TAB;
+    setActiveTabKey(tabKey);
+  }, [location.hash]);
+
+  const handleTabClick = (
+    event: React.MouseEvent<HTMLElement, MouseEvent> | undefined,
+    tabKey: number | string,
+  ) => {
+    const tabKeyStr = String(tabKey);
+    setActiveTabKey(tabKeyStr);
+    window.location.hash = tabKeyStr;
+  };
+
+  return (
+    <>
+      <ClustersPageHeader
+        someReadOnly={someReadOnly}
+        showSpinner={showSpinner}
+        error={error}
+        refresh={refresh}
+      />
+      <Tabs activeKey={activeTabKey} onSelect={handleTabClick} role="region" aria-label="Clusters">
+        <Tab
+          eventKey="list"
+          title={<TabTitleText>Cluster List</TabTitleText>}
+          aria-label="Default content - Cluster List"
+        >
+          List of clusters
+        </Tab>
+        <Tab
+          eventKey="requests"
+          title={<TabTitleText>Cluster Requests</TabTitleText>}
+          aria-label="Default content - Cluster Requests"
+        >
+          Cluster requests
+        </Tab>
+      </Tabs>
+    </>
+  );
+};
