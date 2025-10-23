2f6aaaf0e19da48f5a9292c55b7f9e48eb986099
zherman0 <zherman@redhat.com>
2025-10-23 17:35:28 -0600

    Create new access request look

--- src/components/clusters/ClusterDetailsMultiRegion/components/AccessRequest/AccessRequest.tsx
+++ src/components/clusters/ClusterDetailsMultiRegion/components/AccessRequest/AccessRequest.tsx
@@ -1,14 +1,28 @@
 import React, { useCallback, useMemo } from 'react';
-import { useDispatch } from 'react-redux';
+import { shallowEqual, useDispatch } from 'react-redux';
 
-import { Card, CardBody, CardTitle } from '@patternfly/react-core';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Flex,
+  FlexItem,
+  PageSection,
+  Popover,
+} from '@patternfly/react-core';
+import OutlinedQuestionCircleIcon from '@patternfly/react-icons/dist/esm/icons/outlined-question-circle-icon';
 import { SortByDirection } from '@patternfly/react-table';
 
-import links from '~/common/installLinks.mjs';
+import installLinks from '~/common/installLinks.mjs';
+import useOrganization from '~/components/CLILoginPage/useOrganization';
 import ExternalLink from '~/components/common/ExternalLink';
 import ConnectedModal from '~/components/common/Modal/ConnectedModal';
 import { modalActions } from '~/components/common/Modal/ModalActions';
 import modals from '~/components/common/Modal/modals';
+import { useSetFilteredTotal } from '~/hooks/useSetFilteredTotal';
 import {
   refetchAccessRequests,
   useFetchAccessRequests,
@@ -25,33 +39,49 @@ import AccessRequestTablePagination from './components/AccessRequestTablePaginat
 
 import './AccessRequest.scss';
 
+type AccessRequestVariant = 'page' | 'card';
+
 type AccessRequestProps = {
+  variant?: AccessRequestVariant;
   subscriptionId?: string;
+  showClusterName?: boolean;
 };
 
-const AccessRequest = ({ subscriptionId }: AccessRequestProps) => {
+const DESCRIPTION_TEXT =
+  'Access requests to customer data on Red Hat OpenShift Service on AWS clusters and the corresponding cloud accounts can be created by SRE either in response to a customer-initiated support ticket or in response to alerts received by SRE, as part of the standard incident response process.';
+
+export const AccessRequest = ({
+  variant = 'card',
+  subscriptionId,
+  showClusterName = false,
+}: AccessRequestProps) => {
   const dispatch = useDispatch();
   const viewType = viewConstants.ACCESS_REQUESTS_VIEW;
-  const viewOptions = useGlobalState((state) => state.viewOptions[viewType]);
+  const viewOptions = useGlobalState((state) => state.viewOptions[viewType], shallowEqual);
+  const { organization } = useOrganization();
 
-  const { data: accessRequests, isLoading: isAccessRequestsLoading } = useFetchAccessRequests(
-    subscriptionId!!,
-    viewOptions,
-    false,
-    { enabled: true },
-  );
+  const { data: accessRequests, isLoading: isAccessRequestsLoading } = useFetchAccessRequests({
+    subscriptionId,
+    organizationId: !subscriptionId ? organization?.id : undefined,
+    params: viewOptions,
+    isAccessProtectionLoading: false,
+    accessProtection: { enabled: true },
+  });
 
-  const isPending = useMemo(() => isAccessRequestsLoading, [isAccessRequestsLoading]);
   const isPendingNoData = useMemo(
-    () => isPending || !accessRequests?.items?.length,
-    [isPending, accessRequests],
+    () => isAccessRequestsLoading || !accessRequests?.length,
+    [isAccessRequestsLoading, accessRequests],
   );
+
+  // Update total count after filtering
+  useSetFilteredTotal(accessRequests, isAccessRequestsLoading, viewOptions.totalCount, viewType);
+
   const sortBy = useMemo(
     () => ({
       index: viewOptions.sorting.sortIndex,
       direction: viewOptions.sorting.isAscending ? SortByDirection.asc : SortByDirection.desc,
     }),
-    [viewOptions],
+    [viewOptions.sorting.sortIndex, viewOptions.sorting.isAscending],
   );
 
   const setSorting = useCallback(
@@ -72,49 +102,96 @@ const AccessRequest = ({ subscriptionId }: AccessRequestProps) => {
     [dispatch],
   );
 
+  const readMoreLink = (
+    <ExternalLink href={installLinks.ACCESS_REQUEST_DOC_LINK}>
+      Read more about Access Requests functionality
+    </ExternalLink>
+  );
+
+  const tableContent = (
+    <AccessRequestTable
+      accessRequestItems={accessRequests}
+      setSorting={setSorting}
+      openDetailsAction={openAccessRequest}
+      sortBy={sortBy}
+      isPending={isAccessRequestsLoading}
+      showClusterName={showClusterName}
+    />
+  );
+
+  const pagination = (paginationVariant: 'top' | 'bottom') => (
+    <AccessRequestTablePagination
+      viewType={viewType}
+      viewOptions={viewOptions}
+      variant={paginationVariant}
+      isDisabled={isPendingNoData}
+    />
+  );
+
   return (
     <>
-      <Card className="ocm-c-access-request__card">
-        <CardTitle className="ocm-c-access-request__card--header">Access Requests</CardTitle>
-        <CardBody className="ocm-c-access-request__card--body">
-          <div className="access-request-subtitle">
-            Access requests to customer data on Red Hat OpenShift Service on AWS clusters and the
-            corresponding cloud accounts can be created by SRE either in response to a
-            customer-initiated support ticket or in response to alerts received by SRE, as part of
-            the standard incident response process.
-          </div>
-          <ExternalLink href={links.ACCESS_REQUEST_DOC_LINK}>
-            Read more about Access Requests functionality
-          </ExternalLink>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <AccessRequestTablePagination
-            viewType={viewType}
-            viewOptions={viewOptions}
-            variant="top"
-            isDisabled={isPendingNoData}
-          />
+      {variant === 'page' && (
+        <PageSection hasBodyWrapper={false}>
+          <PageSection hasBodyWrapper={false} hasShadowBottom hasShadowTop>
+            <Flex>
+              <FlexItem grow={{ default: 'grow' }}>
+                <span>Cluster access requests</span>
+                <Popover
+                  bodyContent={
+                    <Flex>
+                      <FlexItem>
+                        <p>{DESCRIPTION_TEXT}</p>
+                      </FlexItem>
+                    </Flex>
+                  }
+                  footerContent={
+                    <Flex>
+                      <FlexItem>
+                        <p>{readMoreLink}</p>
+                      </FlexItem>
+                    </Flex>
+                  }
+                  enableFlip={false}
+                >
+                  <Button icon={<OutlinedQuestionCircleIcon />} variant="plain" />
+                </Popover>
+              </FlexItem>
+            </Flex>
+            <Card>
+              <CardHeader>
+                <Flex>
+                  <FlexItem align={{ default: 'alignRight' }}>{pagination('top')}</FlexItem>
+                </Flex>
+              </CardHeader>
+              <CardBody>
+                {tableContent}
+                <CardFooter>{pagination('bottom')}</CardFooter>
+              </CardBody>
+            </Card>
+          </PageSection>
+        </PageSection>
+      )}
 
-          <AccessRequestTable
-            accessRequestItems={accessRequests?.items}
-            setSorting={setSorting}
-            openDetailsAction={openAccessRequest}
-            sortBy={sortBy}
-            isPending={isPending}
-          />
-          <AccessRequestTablePagination
-            viewType={viewType}
-            viewOptions={viewOptions}
-            variant="bottom"
-            isDisabled={isPendingNoData}
-          />
-        </CardBody>
-      </Card>
+      {variant === 'card' && (
+        <>
+          <Card className="ocm-c-access-request__card">
+            <CardTitle className="ocm-c-access-request__card--header">Access Requests</CardTitle>
+            <CardBody className="ocm-c-access-request__card--body">
+              <div className="access-request-subtitle">{DESCRIPTION_TEXT}</div>
+              {readMoreLink}
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              {pagination('top')}
+              {tableContent}
+              {pagination('bottom')}
+            </CardBody>
+          </Card>
+        </>
+      )}
+
       <ConnectedModal ModalComponent={AccessRequestModalForm} />
     </>
   );
 };
-
-export default AccessRequest;
