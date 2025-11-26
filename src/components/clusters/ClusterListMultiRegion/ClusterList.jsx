702b6c28e3222a4fb692b6599358fd32b90522c1
zherman0 <zherman@redhat.com>
2025-11-26 16:58:47 -0700

    Fix title on browser tab ocmui-3968

--- src/components/clusters/ClusterListMultiRegion/ClusterList.jsx
+++ src/components/clusters/ClusterListMultiRegion/ClusterList.jsx
@@ -80,8 +80,6 @@ import ViewOnlyMyClustersToggle from './components/ViewOnlyMyClustersToggle';
 
 import './ClusterList.scss';
 
-const PAGE_TITLE = 'Cluster List | Red Hat OpenShift Cluster Manager';
-
 const ClusterListPageHeader = ({ someReadOnly, showSpinner, error, refresh }) => (
   <>
     <ReadOnlyBanner someReadOnly={someReadOnly} />
@@ -143,6 +141,9 @@ const ClusterList = ({
 }) => {
   const dispatch = useDispatch();
   const viewType = viewConstants.CLUSTERS_VIEW;
+  const PAGE_TITLE = showTabbedView
+    ? 'Clusters | Red Hat OpenShift Cluster Manager'
+    : 'Cluster List | Red Hat OpenShift Cluster Manager';
 
   /* Get Access Request / Protection Data */
   const { enabled: isOrganizationAccessProtectionEnabled } = useGetAccessProtection(
