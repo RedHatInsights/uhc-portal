992728bf0f5398a9f7194dddec42a9ff54b67362
zherman0 <zherman@redhat.com>
2025-11-26 15:04:59 -0700

    Add pagination to the cluster transfer list table

--- src/queries/ClusterDetailsQueries/ClusterTransferOwnership/useFetchClusterTransfer.tsx
+++ src/queries/ClusterDetailsQueries/ClusterTransferOwnership/useFetchClusterTransfer.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { useQuery } from '@tanstack/react-query';
 
 import { queryClient } from '~/components/App/queryClient';
 import { formatErrorData } from '~/queries/helpers';
 import { queryConstants } from '~/queries/queriesConstants';
+import { onSetTotal } from '~/redux/actions/viewOptionsActions';
+import { viewConstants } from '~/redux/constants';
+import { useGlobalState } from '~/redux/hooks';
 import { accountsService } from '~/services';
 import { ClusterTransferStatus } from '~/types/accounts_mgmt.v1';
 
@@ -23,6 +29,11 @@ export const useFetchClusterTransfer = ({
   filter?: string;
   showPendingTransfer?: boolean;
 }) => {
+  const viewType = viewConstants.CLUSTER_TRANSFER_VIEW;
+  const viewOptions = useGlobalState((state) => state.viewOptions[viewType]);
+
+  const dispatch = useDispatch();
+
   const { data, isLoading, isError, error } = useQuery({
     queryKey: [
       queryConstants.FETCH_CLUSTER_DETAILS_QUERY_KEY,
@@ -30,17 +41,36 @@ export const useFetchClusterTransfer = ({
       filter || clusterExternalID || transferID,
     ],
     queryFn: async () => {
-      if (filter) {
-        return accountsService.searchClusterTransfers(filter);
-      }
       if (clusterExternalID) {
         return accountsService.getClusterTransferByExternalID(clusterExternalID);
       }
-      return accountsService.searchClusterTransfers(`id='${transferID}'`);
+
+      return accountsService.searchClusterTransfers({
+        filter: filter || `id='${transferID}'`,
+        page: viewOptions.currentPage || 1,
+        size: viewOptions.pageSize || 20,
+        orderBy: viewOptions.sorting.sortField
+          ? `${viewOptions.sorting.sortField} ${viewOptions.sorting.isAscending ? 'asc' : 'desc'}`
+          : 'updated_at desc',
+      });
     },
     enabled: !!clusterExternalID || !!transferID || !!filter,
     refetchInterval: queryConstants.STALE_TIME_60_SEC,
   });
+
+  // Update Redux state with total count when data changes
+  useEffect(() => {
+    if (!data?.data || !data.data.total) return;
+    dispatch(onSetTotal(data.data.total, viewType));
+  }, [
+    data?.data,
+    dispatch,
+    viewOptions?.totalCount,
+    viewOptions?.pageSize,
+    viewOptions?.currentPage,
+    viewOptions?.totalPages,
+    viewType,
+  ]);
 
   if (isError) {
     const formattedError = formatErrorData(isLoading, isError, error);
@@ -51,6 +81,7 @@ export const useFetchClusterTransfer = ({
       error: formattedError,
     };
   }
+
   const pendingTransfer =
     data?.data?.items?.find(
       (transfer) =>
