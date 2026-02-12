9649f599d3b69b5f1516713454ae651043c17c6c
Trevor <thendric@redhat.com>
2026-02-12 00:44:21 -0500

    OCMUI-4181: Refactor OCMRoleStep test without Formik wrapper

--- src/components/clusters/wizards/rosa/AccountsRolesScreen/AssociateAWSAccountDrawer/__tests__/OCMRoleStep.test.tsx
+++ src/components/clusters/wizards/rosa/AccountsRolesScreen/AssociateAWSAccountDrawer/__tests__/OCMRoleStep.test.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Formik } from 'formik';
 
 import links from '~/common/installLinks.mjs';
-import { FieldId } from '~/components/clusters/wizards/rosa/constants';
 import { render, screen } from '~/testUtils';
 
 import OCMRoleStep from '../OCMRoleStep';
 
-const buildTestComponent = (formValues = {}) => (
-  <Formik
-    initialValues={{
-      [FieldId.Hypershift]: 'true',
-      ...formValues,
-    }}
-    onSubmit={jest.fn()}
-  >
-    <OCMRoleStep title="" />
-  </Formik>
-);
-
 describe('<OCMRoleStep />', () => {
   it('AWS account association link is correct when hypershift is selected', async () => {
-    const { user } = render(buildTestComponent());
+    const { user } = render(<OCMRoleStep title="" isHypershiftSelected />);
 
     const moreInfoBtn = await screen.findByLabelText(
       'More information on Why do I need to link my account?',
@@ -35,7 +21,7 @@ describe('<OCMRoleStep />', () => {
   });
 
   it('AWS account association link is correct when classic is selected', async () => {
-    const { user } = render(buildTestComponent({ [FieldId.Hypershift]: 'false' }));
+    const { user } = render(<OCMRoleStep title="" isHypershiftSelected={false} />);
 
     const moreInfoBtn = await screen.findByLabelText(
       'More information on Why do I need to link my account?',
