525877aefb099bf033687d81c1d2d578de382098
zherman0 <zherman@redhat.com>
2026-02-18 16:22:58 -0700

    Add undefined check on maxOSVersion

--- src/components/clusters/wizards/rosa/ClusterSettings/VersionSelection.tsx
+++ src/components/clusters/wizards/rosa/ClusterSettings/VersionSelection.tsx
@@ -98,8 +98,10 @@ function VersionSelection({
   const isValidRosaVersion = React.useCallback(
     (version: Version) =>
       version.rosa_enabled &&
-      (isHypershiftSelected || isSupportedMinorVersion(version?.raw_id || '', rosaMaxOSVersion)),
-    [rosaMaxOSVersion, isHypershiftSelected], // rosaMaxOSVersion is actually the max version allowed by the chosen AccountRoles,
+      (isHypershiftSelected ||
+        !rosaMaxOSVersion ||
+        isSupportedMinorVersion(version?.raw_id || '', rosaMaxOSVersion)),
+    [rosaMaxOSVersion, isHypershiftSelected],
   );
 
   const toggleCompatibleVersions = (
