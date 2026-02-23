66eb071dd88c19c3fce36248573f2d9d2aec205b
zherman0 <zherman@redhat.com>
2026-02-23 14:04:05 -0700

    Change the hyperlink to just the link, not the entire sentense.

--- src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields/ServiceAccountAuth/ServiceAccountPrerequisites.tsx
+++ src/components/clusters/wizards/osd/ClusterSettings/CloudProvider/GcpByocFields/ServiceAccountAuth/ServiceAccountPrerequisites.tsx
@@ -101,8 +101,9 @@ const ServiceAccountPrerequisites = () => (
         recommend that you have no other resources provisioned in the project prior to provisioning
         OpenShift Dedicated.
         <br />
+        For more guidance, see the{' '}
         <ExternalLink noIcon href={docLinks.OSD_CCS_GCP}>
-          For more guidance, see theCustomer Cloud Subscription requirements
+          Customer Cloud Subscription requirements
         </ExternalLink>
         .
       </Content>
