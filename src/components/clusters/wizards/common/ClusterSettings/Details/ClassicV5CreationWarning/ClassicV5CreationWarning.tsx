e770b2427216ddebe2ce216c396f7e8c988abe4a
Sonia <sakkiang@redhat.com>
2026-08-19 13:49:32 -0400

    remove duplicate checking logic

--- src/components/clusters/wizards/common/ClusterSettings/Details/ClassicV5CreationWarning/ClassicV5CreationWarning.tsx
+++ src/components/clusters/wizards/common/ClusterSettings/Details/ClassicV5CreationWarning/ClassicV5CreationWarning.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import { Alert } from '@patternfly/react-core';
 
-import { subscriptionCapabilities } from '~/common/subscriptionCapabilities';
+import { hasAllowOcp5Capability } from '~/common/subscriptionCapabilities';
 import InternalTrackingLink from '~/components/common/InternalTrackingLink';
 import { OCP5_SUPPORT } from '~/queries/featureGates/featureConstants';
 import { useFeatureGate } from '~/queries/featureGates/useFetchFeatureGate';
 import { useGlobalState } from '~/redux/hooks/useGlobalState';
-import { Capability } from '~/types/accounts_mgmt.v1';
 
 type ClassicV5CreationWarningProps = {
   isClassic: boolean;
@@ -33,11 +32,7 @@ const getWarningTitle = (product: 'rosa' | 'osd'): React.ReactNode => {
 export const ClassicV5CreationWarning = ({ isClassic, product }: ClassicV5CreationWarningProps) => {
   const isOcp5SupportEnabled = useFeatureGate(OCP5_SUPPORT);
   const organization = useGlobalState((state) => state.userProfile.organization.details);
-  const hasOcp5Capability = (organization?.capabilities ?? []).some(
-    (capability: Capability) =>
-      capability.name === subscriptionCapabilities.ROSA_OSD_ALLOW_OCP_5 &&
-      capability.value === 'true',
-  );
+  const hasOcp5Capability = hasAllowOcp5Capability(organization?.capabilities);
 
   if (!isOcp5SupportEnabled || !isClassic || hasOcp5Capability) {
     return null;
