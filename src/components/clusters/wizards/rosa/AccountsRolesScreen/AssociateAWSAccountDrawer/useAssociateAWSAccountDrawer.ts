b754d634f4413b082eb9c8e1a0f777135a214e8d
Netta Meiri <nmeiri@redhat.com>
2026-08-27 14:44:18 +0300

    OCMUI-4181: Integrate Chrome drawerActions API in ROSA wizard Accounts and roles step

---
+++ src/components/clusters/wizards/rosa/AccountsRolesScreen/AssociateAWSAccountDrawer/useAssociateAWSAccountDrawer.ts
@@ -0,0 +1,78 @@
+import { useCallback, useRef } from 'react';
+
+import { trackEvents } from '~/common/analytics';
+import useAnalytics from '~/hooks/useAnalytics';
+import { useChromeDrawerPanel } from '~/hooks/useChromeDrawerPanel';
+
+import { AWSAccountRole } from './common/AssociateAWSAccountStep';
+import {
+  buildAssociateAWSAccountDrawerContent,
+  getAssociateAWSAccountDrawerTitle,
+} from './AssociateAWSAccountDrawerContent';
+
+export type OpenAssociateAWSAccountDrawerArgs = {
+  targetRole?: AWSAccountRole;
+  onClose?: () => void;
+};
+
+export type OpenAssociateAWSAccountDrawer = (args?: OpenAssociateAWSAccountDrawerArgs) => void;
+
+type CloseAssociateAWSAccountDrawerArgs = {
+  skipOnClose?: boolean;
+};
+
+type CloseAssociateAWSAccountDrawer = (args?: CloseAssociateAWSAccountDrawerArgs) => void;
+
+export const useAssociateAWSAccountDrawer = (isHypershiftSelected: boolean) => {
+  const track = useAnalytics();
+  const openOnCloseRef = useRef<(() => void) | undefined>(undefined);
+  const skipNextOnCloseRef = useRef(false);
+
+  const handleDrawerClosed = useCallback(() => {
+    if (skipNextOnCloseRef.current) {
+      skipNextOnCloseRef.current = false;
+      openOnCloseRef.current = undefined;
+      return;
+    }
+
+    openOnCloseRef.current?.();
+    openOnCloseRef.current = undefined;
+  }, []);
+
+  const { open, close } = useChromeDrawerPanel({
+    module: './DrawerPanel',
+    onClose: handleDrawerClosed,
+  });
+
+  const closeDrawer: CloseAssociateAWSAccountDrawer = useCallback(
+    (args = {}) => {
+      const { skipOnClose = false } = args;
+      if (skipOnClose) {
+        skipNextOnCloseRef.current = true;
+      }
+      close();
+    },
+    [close],
+  );
+
+  const openDrawer: OpenAssociateAWSAccountDrawer = useCallback(
+    (args = {}) => {
+      const { targetRole, onClose } = args;
+
+      track(trackEvents.AssociateAWS);
+      openOnCloseRef.current = onClose;
+
+      open({
+        title: getAssociateAWSAccountDrawerTitle(targetRole),
+        content: buildAssociateAWSAccountDrawerContent({
+          targetRole,
+          isHypershiftSelected,
+          onClose: close,
+        }),
+      });
+    },
+    [close, isHypershiftSelected, open, track],
+  );
+
+  return { openDrawer, closeDrawer };
+};
