02e778d587484a5317bf89eb5ddfc50645886f60
Netta Meiri <nmeiri@redhat.com>
2026-08-27 14:44:18 +0300

    OCMUI-4181: Integrate Chrome drawerActions API in ROSA wizard Accounts and roles step

--- src/components/clusters/wizards/rosa/AccountsRolesScreen/AccountsRolesScreen.tsx
+++ src/components/clusters/wizards/rosa/AccountsRolesScreen/AccountsRolesScreen.tsx
@@ -23,7 +23,7 @@ import { isRestrictedEnv } from '~/restrictedEnv';
 import { FieldId } from '../constants';
 
 import AccountRolesARNsSection from './AccountRolesARNsSection/AccountRolesARNsSection';
-import { useAssociateAWSAccountDrawer } from './AssociateAWSAccountDrawer/AssociateAWSAccountDrawer';
+import { OpenAssociateAWSAccountDrawer } from './AssociateAWSAccountDrawer/useAssociateAWSAccountDrawer';
 import AWSBillingAccount from './AWSBillingAccount/AWSBillingAccount';
 import AWSAccountSelection from './AWSAccountSelection';
 import { AwsRoleErrorAlert } from './AwsRoleErrorAlert';
@@ -46,6 +46,7 @@ export interface AccountsRolesScreenProps {
   organizationID: string;
   isHypershiftEnabled: boolean;
   isHypershiftSelected: boolean;
+  openDrawer: OpenAssociateAWSAccountDrawer;
   onContractCheckChange?: (hasWarning: boolean) => void;
   isContractDialogOpen?: boolean;
   onContractDialogContinue?: () => void;
@@ -64,6 +65,7 @@ function AccountsRolesScreen({
   clearGetUserRoleResponse,
   isHypershiftEnabled,
   isHypershiftSelected,
+  openDrawer,
   onContractCheckChange,
   isContractDialogOpen,
   onContractDialogContinue,
@@ -96,7 +98,6 @@ function AccountsRolesScreen({
   const openDrawerButtonRef = useRef(null);
   const hasAWSAccounts = AWSAccountIDs.length > 0;
   const track = useAnalytics();
-  const { openDrawer } = useAssociateAWSAccountDrawer(isHypershiftSelected);
 
   const machineTypesByRegion = useSelector((state: GlobalState) => state.machineTypesByRegion);
   const dispatch = useDispatch();
@@ -174,8 +175,14 @@ function AccountsRolesScreen({
   }, [getUserRoleResponse?.error, noUserForSelectedAWSAcct]);
 
   const onClick = useCallback(
-    (event: any) => {
-      openDrawer({ focusOnClose: event.target, onClose: clearGetAWSAccountIDsResponse });
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      const focusTarget = event.currentTarget;
+      openDrawer({
+        onClose: () => {
+          clearGetAWSAccountIDsResponse();
+          focusTarget.focus();
+        },
+      });
     },
     [clearGetAWSAccountIDsResponse, openDrawer],
   );
@@ -239,6 +246,7 @@ function AccountsRolesScreen({
             isLoading={refreshButtonClicked && isAWSDataPending}
             isDisabled={isAWSDataPending}
             clearGetAWSAccountIDsResponse={clearGetAWSAccountIDsResponse}
+            openDrawer={openDrawer}
           />
           <Button
             variant="secondary"
@@ -255,6 +263,7 @@ function AccountsRolesScreen({
           <AWSBillingAccount
             selectedAWSBillingAccountID={selectedAWSBillingAccountID || ''}
             selectedAWSAccountID={selectedAWSAccountID || ''}
+            openDrawer={openDrawer}
             onContractCheckChange={onContractCheckChange}
             isContractDialogOpen={isContractDialogOpen}
             onContractDialogContinue={onContractDialogContinue}
@@ -270,6 +279,7 @@ function AccountsRolesScreen({
             getAWSAccountRolesARNsResponse={getAWSAccountRolesARNsResponse}
             clearGetAWSAccountRolesARNsResponse={clearGetAWSAccountRolesARNsResponse}
             isHypershiftSelected={isHypershiftSelected}
+            openDrawer={openDrawer}
             onAccountChanged={resetUserRoleFields}
             onOCMRoleRefresh={resetAWSAccountFields}
           />
@@ -277,7 +287,11 @@ function AccountsRolesScreen({
 
         {(getUserRoleResponse?.error || noUserForSelectedAWSAcct) && (
           <GridItem span={8} className="pf-v6-u-mt-sm">
-            <AwsRoleErrorAlert title="A user-role could not be detected" targetRole="user" />
+            <AwsRoleErrorAlert
+              openDrawer={openDrawer}
+              title="A user-role could not be detected"
+              targetRole="user"
+            />
           </GridItem>
         )}
       </Grid>
