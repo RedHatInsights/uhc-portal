0b43168bf334078e0f67c8a21a0a9b8a3a3c6eac
Netta Meiri <nmeiri@redhat.com>
2026-09-16 12:57:23 +0300

    OCMUI-5365: Fix broken external auth links

--- src/common/docLinks.mjs
+++ src/common/docLinks.mjs
@@ -78,6 +78,8 @@ const docLinks = {
     'https://registry.terraform.io/providers/terraform-redhat/rhcs/latest/docs/guides/hosted-control-planes',
   ROSA_AWS_FEDRAMP: `${ROSA_DOCS_BASE}/getting_started_with_red_hat_openshift_service_on_aws_in_aws_govcloud/index`,
   ROSA_HCP_CLI_URL: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-quickly`,
+  ROSA_HCP_EXT_AUTH: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-ext-auth`,
+  ROSA_HCP_BREAK_GLASS: `${ROSA_DOCS_BASE}/install_clusters/rosa-hcp-sts-creating-a-cluster-ext-auth#rosa-hcp-sts-accessing-a-break-glass-cred-cli_rosa-hcp-sts-creating-a-cluster-ext-auth`,
   LEARN_MORE_SSO_ROSA: `${ROSA_DOCS_BASE}/cli_tools/rosa-cli#rosa-login-sso_rosa-getting-started-cli`,
   ROSA_AUTONODE: `${ROSA_DOCS_BASE}-single/cluster_administration/index#rosa-nodes-autonode-managing`,
 
